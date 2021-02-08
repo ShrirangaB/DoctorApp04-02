33b1fda2367505582bc16d8f51566251cfb8fe5a
ShrirangaB <77617450+ShrirangaB@users.noreply.github.com>
2021-02-08 18:10:54 +0530

    Add files via upload

---
+++ dbscreen.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:networkJsonAPp/model/empmodel.dart';
+
+import 'package:networkJsonAPp/service/empProvider.dart';
+import 'package:networkJsonAPp/service/empdbprovider.dart';
+
+class Employeee extends StatefulWidget {
+  const Employeee({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<Employeee> {
+  var isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Api to sqlite'),
+        centerTitle: true,
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.only(right: 10.0),
+            child: IconButton(
+              icon: Icon(Icons.cloud_download_outlined),
+              onPressed: () async {
+                await _loadFromApi();
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 10.0),
+            child: IconButton(
+              icon: Icon(Icons.update),
+              onPressed: () async {
+                await _updateData();
+              },
+            ),
+          ),
+        ],
+      ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : _buildEmployeeListView(),
+    );
+  }
+
+  _updateData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    await DBProviderForJSON.db.updateData();
+
+    // wait for 1 second to simulate loading of data
+    await Future.delayed(const Duration(seconds: 1));
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  _loadFromApi() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    var apiProvider = EmployeeApiProvider();
+    await apiProvider.getAllEmployees();
+
+    // wait for 2 seconds to simulate loading of data
+    await Future.delayed(const Duration(seconds: 2));
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  _buildEmployeeListView() {
+    return FutureBuilder(
+      future: DBProviderForJSON.db.getAllEmployees(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          if (snapshot.data != null) {
+            List<Employee> list = snapshot.data;
+            return ListView.separated(
+              separatorBuilder: (context, index) => Divider(
+                color: Colors.black12,
+              ),
+              itemCount: list.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: Text(
+                    "${index + 1}",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  title: Text(
+                      "Name: ${list[index].name}\nUserName: ${list[index].username}\nEmail: ${list[index].email}\nStreet: ${list[index].address.street}\nSuite :${list[index].address.suite}\nCity :${list[index].address.city}\nZip: ${list[index].address.zipcode}"),
+                  // subtitle: Text(
+                  //     'Street: ${list[index].address.street}\nSuite :${list[index].address.suite}\nCity :${list[index].address.city}\nZip: ${list[index].address.zipcode}'),
+                );
+              },
+            );
+          }
+        }
+      },
+    );
+  }
+}
+// return ListView.separated(
+//   separatorBuilder: (context, index) => Divider(
+//     color: Colors.black12,
+//   ),
+//   itemCount: snapshot.data.length,
+//   itemBuilder: (BuildContext context, int index) {
+//     return ListTile(
+//       leading: Text(
+//         "${index + 1}",
+//         style: TextStyle(fontSize: 20.0),
+//       ),
+//       title: Text(
+//           "Name: ${snapshot.data[index].name}\nUsername: ${snapshot.data[index].username} "),
+//       subtitle: Text(
+//           'EMAIL: ${snapshot.data[index].email}\nCity: ${snapshot.data[index].city}'),
+//       // \nStreet: ${snapshot.data[index].street}\nCity: ${snapshot.data[index].city}'),
+//     );
+//   },
+// );
---
+++ empdbprovider.dart
@@ -0,0 +1,107 @@
+// import 'dart:html';
+import 'dart:io';
+
+import 'package:networkJsonAPp/model/empmodel.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBProviderForJSON {
+  final cityname = 'Mumbai';
+  static Database _database;
+  static final DBProviderForJSON db = DBProviderForJSON._();
+  DBProviderForJSON._();
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await initDB();
+    return _database;
+  }
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'employee_manager.db');
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+      await db.execute('CREATE TABLE Employee('
+          'id INTEGER PRIMARY KEY,'
+          'email TEXT,'
+          'username TEXT,'
+          'name TEXT'
+          ')');
+      await db.execute('CREATE TABLE Adress('
+          'id INTEGER,'
+          'street TEXT,'
+          'suite TEXT,'
+          'city TEXT,'
+          'zipcode TEXT,'
+          'FOREIGN KEY (id) REFERENCES Employee(id)'
+          ')');
+    });
+  }
+
+  createEmployee(Employee newEmployee) async {
+    await deleteAllEmployees();
+    // final db = await database;
+    // final res = await db.insert('Employee', newEmployee.toJson());
+
+    //  return res;
+
+    final db = await database;
+    final res = await db.insert('Employee', {
+      'id': newEmployee.id,
+      'email': newEmployee.email,
+      'username': newEmployee.username,
+      'name': newEmployee.name,
+      // 'phone':newEmployee.phone,
+    });
+
+    Address newAddress = newEmployee.address;
+    final addressResult = await db.insert('Adress', {
+      'city': newAddress.city,
+      'street': newAddress.street,
+      'suite': newAddress.suite,
+      'zipcode': newAddress.zipcode,
+      'id': newEmployee.id,
+    });
+    return res;
+
+    //return addressResult;
+  }
+
+  Future<int> deleteAllEmployees() async {
+    final db = await database;
+    final res = await db.rawDelete('DELETE FROM Employee');
+
+    return res;
+  }
+
+  Future<int> updateData() async {
+    final db = await database;
+    final result = await db.rawUpdate(
+        'UPDATE Adress SET Adress.city = $cityname WHERE Adress.id = 2');
+    return result;
+  }
+
+  Future<List<Employee>> getAllEmployees() async {
+    final db = await database;
+    final res = await db.rawQuery
+        //("SELECT * FROM EMPLOYEE");
+        ("SELECT Employee.id,Employee.email,Employee.username,Employee.name,Adress.street,Adress.suite,Adress.city,Adress.zipcode FROM Employee INNER JOIN Adress ON Employee.id=Adress.id");
+
+    List<Employee> list = res.isNotEmpty
+        ? res.map((c) {
+            print('res.map $c');
+
+            final employee = Employee.fromJson(c);
+            employee.address = Address.fromJson(c);
+
+            return employee;
+          }).toList()
+        : [];
+    //=> Employee.fromJson(c)).toList() : [];
+    print(list);
+
+    return list;
+  }
+}
